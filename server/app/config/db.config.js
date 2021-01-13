76267650b2337d761e311b48bf039416b1f74b86
SeanLewisIRE <slewis100@gmail.com>
2021-01-13 19:38:14 +0000

    Changes to config to detect ENV vars

--- server/app/config/db.config.js
+++ server/app/config/db.config.js
@@ -1,19 +1,41 @@
-const creds = require('../../serverCredentials')
+if (process.env.LIVE) {
 
-module.exports = {
-    DB: `${creds.RDS_DB}`,
-    HOST: `${creds.RDS_HOST}`,
-    USER: `${creds.RDS_USER}`,
-    PASSWORD: `${creds.RDS_PASSWORD}`,
-    PORT: `${creds.RDS_PORT}`,
-    dialect: "mysql",
-    dialectOptions: {
-        ssl: true
-    },
-    pool: {
-        max: 5,
-        min: 0,
-        acquire: 30000,
-        idle: 10000
-    }
-};
+    module.exports = {
+        DB: process.env.RDS_DB,
+        HOST: process.env.RDS_HOST,
+        USER: process.env.RDS_USER,
+        PASSWORD: process.env.RDS_PASSWORD,
+        PORT: process.env.RDS_PORT,
+        dialect: "mysql",
+        dialectOptions: {
+            ssl: true
+        },
+        pool: {
+            max: 5,
+            min: 0,
+            acquire: 30000,
+            idle: 10000
+        }
+    };
+
+} else {
+    const creds = require('../../serverCredentials')
+
+    module.exports = {
+        DB: `${creds.RDS_DB}`,
+        HOST: `${creds.RDS_HOST}`,
+        USER: `${creds.RDS_USER}`,
+        PASSWORD: `${creds.RDS_PASSWORD}`,
+        PORT: `${creds.RDS_PORT}`,
+        dialect: "mysql",
+        dialectOptions: {
+            ssl: true
+        },
+        pool: {
+            max: 5,
+            min: 0,
+            acquire: 30000,
+            idle: 10000
+        }
+    };
+}
