84d2feef1f66db900ba57b346da2469f170ae461
SeanLewisIRE <slewis100@gmail.com>
2021-01-15 21:02:09 +0000

    https > http

--- frontend/src/http-common.js
+++ frontend/src/http-common.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export default axios.create({
-    baseURL: "https://hopsfyi.herokuapp.com/api",
+    baseURL: "http://hopsfyi.herokuapp.com/api",
     headers: {
         "Content-type": "application/json"
     }
