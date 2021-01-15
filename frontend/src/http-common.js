3ad6a523d492a9dfb88b734608afcd9202466edc
SeanLewisIRE <slewis100@gmail.com>
2021-01-15 21:51:17 +0000

    debuggin, added axios

--- frontend/src/http-common.js
+++ frontend/src/http-common.js
@@ -1,8 +1,9 @@
 import axios from "axios";
 
 export default axios.create({
-    baseURL: "https://hopsfyi.herokuapp.com/api",
+    baseURL: "https://hopsfyi.herokuapp.com",
     headers: {
-        "Content-type": "application/json"
+        "Content-type": "application/json",
+        "Access- Control - Allow - Origin": "https://hopsfyi.herokuapp.com/api"
     }
 });
