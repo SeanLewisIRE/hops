3920830953d3106d65fa4414f52756e0f9b4e881
SeanLewisIRE <slewis100@gmail.com>
2021-01-16 18:17:51 +0000

    debug

--- server/server.js
+++ server/server.js
@@ -13,7 +13,7 @@ const corsOptions = {
 }
 
 if (process.env.LIVE) {
-    corsOptions.origin = 'http://hopsfyi.herokuapp.com//api/beers'
+    corsOptions.origin = 'https://hopsfyi.herokuapp.com//api/beers'
 } else {
     corsOptions.origin = 'http://localhost:8080'
 }
