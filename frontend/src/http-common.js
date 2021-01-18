c6fa07a630916a14de4fc8d71c21496bd3b79f3e
SeanLewisIRE <slewis100@gmail.com>
2021-01-18 22:00:20 +0000

    debugging http-common

--- frontend/src/http-common.js
+++ frontend/src/http-common.js
@@ -1,8 +1,10 @@
 
 import axios from "axios";
+require('dotenv').config()
 
 function axiosCreate() {
-    if(process.env.REACT_APP_DEPLOY === 'true') {
+    if (process.env.REACT_APP_DEPLOY === 'true') {
+        console.log("live")
         return axios.create({
             baseURL: "https://hopsfyi.herokuapp.com/api",
             headers: {
@@ -11,7 +13,8 @@ function axiosCreate() {
             }
         });
     } else {
-       return axios.create({
+        console.log("local")
+        return axios.create({
             baseURL: "http://localhost:8080/api",
             headers: {
                 "Content-type": "application/json",
