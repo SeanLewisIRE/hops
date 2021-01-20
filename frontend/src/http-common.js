27815a1b5baf3f412ad59e009d00d5056d1cdffa
SeanLewisIRE <slewis100@gmail.com>
2021-01-20 21:48:58 +0000

    mixed up DEV/PROD envs

--- frontend/src/http-common.js
+++ frontend/src/http-common.js
@@ -3,9 +3,7 @@ import axios from "axios";
 require('dotenv').config()
 
 function axiosCreate() {
-    console.log("This is AXIOS testing")
-    console.log(process.env.NODE_ENV)
-    if (process.env.NODE_ENV === 'development') {
+    if (process.env.NODE_ENV === 'production') {
         console.log("live")
         return axios.create({
             baseURL: "https://hopsfyi.herokuapp.com/api",
@@ -14,7 +12,7 @@ function axiosCreate() {
                 "Access-Control-Allow-Origin": "*"
             }
         });
-    } else {
+    } else if (process.env.NODE_ENV === 'development') {
         console.log("local")
         return axios.create({
             baseURL: "http://localhost:8080/api",
